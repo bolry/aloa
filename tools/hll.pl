a30195effd5fbb847d9713aeaa37a28dcc59fc60
Bo Rydberg <2945606+bolry@users.noreply.github.com>
2019-01-07 22:38:49 +0100

    Tools to work with PC-lint Plus (#1)
    * Modified gcclint and hll to work with PC-lint Plus
    * Rename PCLINT_PATH to PCLP_PATH

--- tools/hll.pl
+++ tools/hll.pl
@@ -4,12 +4,13 @@
 # HLL - Heuristic Lint issue Locator.
 #
 # Prints descriptions for given lint error numbers by extracting parts from
-# PC-Lint's 'msg.txt' file.
+# PC-lint Plus's 'msg.txt' file generated with '-dump_messages(file=msg.txt)'
 #
 # Written 2009 by Ralf Holly (ralf.holly_at_approxion.com).
 # You can freely use and redistribute this script; use it at your own risk!
 #
 # 2009-07-21 V1.10 Small bugfix related to multi-matching (issue number regex)
+# 2019-01-05 Modified to run with PC-lint Plus and its plain generated msg.txt
 ###############################################################################
 
 use strict;
@@ -23,7 +24,7 @@ while (my $issue_number = shift @ARGV) {
     my $found = 0;
     while(<MSG_FILE>) {
         # If lint issue nummer found
-        if (m/^(\d+)\s+\S+/) {
+        if (m/^(?:error|info|note|supplemental|warning) (\d+)$/) {
             # If searched-for issue found
             if ($1 =~ /\b$issue_number\b/) {
                     $found = 1;
@@ -46,23 +47,23 @@ print <<HERE;
 HLL - Heuristic Lint issue Locator.
 
 Prints descriptions for given lint error numbers by extracting parts from
-PC-Lint's 'msg.txt' file.
+PC-lint Plus's 'msg.txt' file. Generate it with '-dump_messages(file=msg.txt)'
 Written 2009 by Ralf Holly (ralf.holly_at_approxion.com).
+Modifed 2019 by Bo Rydberg (bolry_at_hotmail.com).
 You can freely use and redistribute this script; use it at your own risk!
 
 Usage:
-  hll <msg-file> [number] ...
+  hll.pl <msg-file> [number] ...
   msg-file:
-      Path to PC-Lint's 'msg.txt' file
+      Path to PC-lint Plus's 'msg.txt' file
   number:
       Lint error number (Perl regular expressions supported)
 Example:
-    hll.pl c:\\lint\\msg.txt 407 609
-    hll.pl c:\\lint\\msg.txt 5.. 60[0-9]
+    hll.pl /opt/pclp/msg.txt 407 608
+    hll.pl /opt/pclp/msg.txt 5.. 60[0-9]
 Hint:
     In order to save passing the path to your 'msg.txt' file, invoke HLL
     from a shell script, batch file or via an abbreviation.
 HERE
  exit 1;
 }
-
